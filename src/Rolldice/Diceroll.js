7a67a6ba92d42e45440055f29d2342fa7a9e3514
pankaj021 <kssm.pankaj@gmail.com>
2020-04-18 17:34:26 +0530

    win amount calculation

--- src/Rolldice/Diceroll.js
+++ src/Rolldice/Diceroll.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import './Diceroll.css';
 
-let intiValaue = { betAmt: 500,
-    GameStatus : 'About to start',
-    DiceValue : 0,
-    count : 0,
-    selectedValue :undefined,
-    arrayOfValue : [],
-    result : undefined,
-    winAmt : 0
+let intiValaue = {
+    betAmt: 500,
+    GameStatus: 'About to start',
+    DiceValue: 0,
+    count: 0,
+    selectedValue: undefined,
+    arrayOfValue: [],
+    result: undefined,
+    winAmt: 0
 }
-
-
 
 export default class Diceroll extends React.Component {
     constructor() {
@@ -19,107 +18,121 @@ export default class Diceroll extends React.Component {
         this.state = intiValaue
     }
 
-  onClickedBetAmt= (event)=>{
-      console.log("betAmount",event.target.innerText)
-      let {betAmt} = this.state
-      let temp = event.target.innerText
-      if (temp==='-' && betAmt>=500)
-      {     betAmt -=500
-      }else{
-          betAmt +=500}
-          this.setState({betAmt})
-      }
+    onClickedBetAmt = (event) => {
+        console.log("betAmount", event.target.innerText)
+        let {betAmt} = this.state
+        let temp = event.target.innerText
+        if (temp === '-' && betAmt >= 500) {
+            betAmt -= 500
+        } else {
+            betAmt += 500
+        }
+        this.setState({betAmt})
+    }
 
-      onClickedDice = (event) => {
-        let {DiceValue,GameStatus,count,result,arrayOfValue,selectedValue,winAmt} = this.state
-        console.log("Array of value",arrayOfValue)
-        console.log("selecValue clicked dice",selectedValue)
-        if(count<=6 && selectedValue!==undefined)
-        {   GameStatus ='Game in progress'
-            DiceValue = Math.ceil(Math.random()*6)
+    onClickedDice = (event) => {
+        let {
+            DiceValue,
+            GameStatus,
+            count,
+            result,
+            betAmt,
+            arrayOfValue,
+            selectedValue,
+            winAmt
+        } = this.state
+        console.log("Array of value", arrayOfValue)
+        console.log("selecValue clicked dice", selectedValue)
+        if (count <= 6 && selectedValue !== undefined) {
+            GameStatus = 'Game in progress'
+            DiceValue = Math.ceil(Math.random() * 6)
             result = this.winner(DiceValue)
-            
 
-        count +=1}
-        else{
-            GameStatus ='Game is over'}
-            console.log("winning amout before contion checking  ",winAmt)
-             winAmt = (result === 'win' )? winAmt : " -- "
-             console.log("winning amout After contion checking  ",winAmt)
-        this.setState({DiceValue,GameStatus,count,result,winAmt})
-      }
-        
-      onClickedChoice = (event) =>{
-           let {selectedValue,arrayOfValue,betAmt,winAmt} = this.state
-           selectedValue = event.target.innerText
-           if(selectedValue==="1 To 3")
-           {
-               arrayOfValue = [1,2,3]
-               winAmt = betAmt*2
-               console.log("arrayOf Value",arrayOfValue)
-           }else if(selectedValue==="4 To 5")
-              {
-                arrayOfValue = [4,5]
-                winAmt = betAmt*3
-                console.log("arrayOf Value",arrayOfValue)
-              }else{
-
-                arrayOfValue = [6]
-                winAmt = betAmt*4
-                console.log("arrayOf Value",arrayOfValue)
-              }
-                 this.setState({selectedValue,arrayOfValue})
-      }
-      winner(a)
-      { let {arrayOfValue} = this.state
-            for(let i=0;i<arrayOfValue.length;i++)
-            {
-                if(arrayOfValue[i]===a)
-                     return "win"
+            count += 1
+        } else {
+            GameStatus = 'Game is over'
+        }
+        console.log("winning amout before contion checking  ", winAmt)
+        if (result === 'win') {
+            if (selectedValue === "1 To 3") {
+                winAmt = betAmt * 2
+            } else if (selectedValue === "4 To 5") {
+                winAmt = betAmt * 3
+            } else {
+                winAmt = betAmt * 4
             }
-           return 'loss'
-      }
-    
+        } else {
+            winAmt = '--'
+        }
+        console.log("winning amout After contion checking  ", winAmt)
+        this.setState({DiceValue, GameStatus, count, result, winAmt})
+    }
 
-restartGame = (event) => {
+    onClickedChoice = (event) => {
+        let {selectedValue, arrayOfValue, betAmt, winAmt} = this.state
+        selectedValue = event.target.innerText
+        if (selectedValue === "1 To 3") {
+            arrayOfValue = [1, 2, 3]
+            winAmt = betAmt * 2
+            console.log("arrayOf Value", arrayOfValue)
+        } else if (selectedValue === "4 To 5") {
+            arrayOfValue = [4, 5]
+            winAmt = betAmt * 3
+            console.log("arrayOf Value", arrayOfValue)
+        } else {
+
+            arrayOfValue = [6]
+            winAmt = betAmt * 4
+            console.log("arrayOf Value", arrayOfValue)
+        }
+        this.setState({selectedValue, arrayOfValue})
+    }
+    winner(a)
+    {
+        let {arrayOfValue} = this.state
+        for (let i = 0; i < arrayOfValue.length; i++) {
+            if (arrayOfValue[i] === a) 
+                return "win"
+        }
+        return 'loss'
+    }
+
+    restartGame = (event) => {
         this.setState(intiValaue)
     }
-   
-render() {
-        let {result,winAmt} = this.state
-        console.log("state: ",this.state);
+
+    render() {
+        let {result, winAmt} = this.state
+        console.log("state: ", this.state);
         return (
-            
+
             <div className="calculator-wrap tic-tac-wrp">
                 <div className="bet1 ">
-                <button onClick={this.restartGame}>Restart</button>
-                <h4>GameStatus :{this.state.GameStatus} </h4></div>
-                <h4> DiceValue :{this.state.DiceValue  || 0}</h4>
-                <h4> Result :{this.state.result || " -- "}</h4>
-               <h4> Winning :{this.state.winAmt}</h4>
+                    <button onClick={this.restartGame}>Restart</button>
+                    <h4>GameStatus :{this.state.GameStatus}
+                    </h4>
+                </div>
+                <h4>
+                    DiceValue :{this.state.DiceValue || 0}</h4>
+                <h4>
+                    Result :{this.state.result || " -- "}</h4>
+                <h4>
+                    Winning :{this.state.winAmt}</h4>
                 <div className="container">
-                     <div className='display'>
-                           <div
-                            className="btn-cell" onClick={this.onClickedChoice} >1 To 3</div>
-                            <div
-                            className="btn-cell" onClick={this.onClickedChoice} >4 To 5</div>
-                            <div
-                            className="btn-cell" onClick={this.onClickedChoice} >6</div>
-                            </div>
+                    <div className='display'>
+                        <div className="btn-cell" onClick={this.onClickedChoice}>1 To 3</div>
+                        <div className="btn-cell" onClick={this.onClickedChoice}>4 To 5</div>
+                        <div className="btn-cell" onClick={this.onClickedChoice}>6</div>
+                    </div>
 
                     <div className='display'>
-                        <div
-                            className="btn-cell " onClick={this.onClickedBetAmt}>-</div>
-                            <div
-                            className="btn-cell " >{this.state.betAmt || 500}</div>
-                         <div
-                            className="btn-cell " onClick={this.onClickedBetAmt}>+</div>
-                        </div>
-                        </div>
-                         <div
-                            className="btn-cell " onClick={this.onClickedDice}>Roll Dice</div>
-                    
-            
+                        <div className="btn-cell " onClick={this.onClickedBetAmt}>-</div>
+                        <div className="btn-cell ">{this.state.betAmt || 500}</div>
+                        <div className="btn-cell " onClick={this.onClickedBetAmt}>+</div>
+                    </div>
+                </div>
+                <div className="btn-cell " onClick={this.onClickedDice}>Roll Dice</div>
+
             </div>
         )
     }
